5c32558fa9062890053f9391c849f98c8e6e6544
amsilva <andre.marcos@gmail.com>
2021-09-09 01:06:28 +0000

    Initial commit

---
+++ Apoio.java
@@ -0,0 +1,27 @@
+class Apoio {
+
+  public static String converte(int dec) {
+    int resto;
+    int[] vetor = new int[10]; 
+    int i = 0;
+    String bin = "";
+
+      while (dec >= 2) {
+        resto = dec % 2;
+        vetor[i++] = resto;
+        dec = dec / 2;
+      }
+      vetor[i] = dec;
+
+      for (int x=i; x>=0; x--) {
+        //System.out.print(vetor[x]);
+        bin = bin + Integer.toString(vetor[x]);
+      }
+
+      //System.out.print(bin);
+
+      return bin;
+
+  }
+
+}
